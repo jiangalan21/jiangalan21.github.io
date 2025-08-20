f9afc73333c7be3be6bfdefd4795916a8ddee16d
Alan Jiang <jiangalan21@gmail.com>
2025-08-19 20:15:37 -0700

    more progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,130 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center space-y-500">
-    
-      <section className="grid gap-8 md:grid-cols-2 md:items-center text-center md:text-left mt-14" id="home">
-        <div>
-          <img className="w-80 h-80 2xl:w-160 2xl:h-160 rounded-full object-cover" src="/alanjiang.JPEG"/>
-        </div>
-        <div className="">
-          <p>Hello! My name is</p>
-          <h1 className="text-4xl font-medium mt-1 mb-2">Alan Jiang</h1>
-          <div className="flex justify-center md:justify-start">
-            <a href="https://www.linkedin.com/in/alan-jiang21/">
-              <button className="cursor-pointer">LinkedIn</button>
-            </a>
+    <div className="flex flex-col items-center space-y-50">
+
+      <section id="about">
+
+        <div className="min-w-md grid gap-4 md:grid-cols-2 md:gap-12 md:items-center text-center md:text-left mt-25" >
+
+          <div className="">
+            <img className="w-80 h-80 md:place-self-end 2xl:w-110 2xl:h-110 transition-all duration-500 place-self-center rounded-full object-cover" src="/alanjiang.JPEG"/>
+          </div>
+
+          <div className="w-3/5 flex flex-col place-self-center md:max-lg:w-4/5 xl:w-7/10 md:place-self-start md:self-center space-y-5">
+            <h1 className="text-5xl md:text-6xl">Alan Jiang</h1> {/**add some effect? */}
+            <h1 className="text-2xl">Fullstack Engineer</h1>
+            <p>Hello, my name is Alan Jiang, a third-year Computer Science major at UC San Diego.</p>
+
+            <div className="flex space-x-2 justify-center md:justify-start">
+
+              <a href="https://www.linkedin.com/in/alan-jiang21/" target="_blank" rel="noopener noreferrer">
+                <button className="cursor-pointer">LinkedIn</button>
+              </a>
+              <a href="https://github.com/jiangalan21" target="_blank" rel="noopener noreferrer">
+                <button className="cursor-pointer">GitHub</button>
+              </a>
+
+            </div>
+
+            <div className="flex max-md:flex-col space-y-3 md:space-x-3">
+              <a href=""><button className="w-46 border-2 rounded-xl py-3 px-8 font-bold cursor-pointer">Explore my CV</button></a>
+              <a href="mailto:jiangalan21@gmail.com"><button className="w-46 border-2 rounded-xl py-3 px-8 font-bold cursor-pointer">Contact Me</button></a>
+            </div>
+
           </div>
         </div>
       </section>
 
-      <section id="projects">
-        <div>
-          <p>Projects</p>
+      <section className="w-full h-850 mb-40" id="projects">
+
+        <div className="h-full flex flex-col items-center space-y-15 mt-25">
+
+          <h1 className="text-6xl font-bold">Projects</h1>
+
+          <div className="w-4/8 grid gap-y-10 md:grid-cols-2 justify-items-center">
+            <a href="https://akpsiucsd.com" target="_blank" rel="noopener noreferrer" 
+            className="w-3/4 min-h-1/6 max-w-150 border-2 rounded-xl hover:scale-105 transition duration-500 delay-75">
+
+              <div className="h-full flex flex-col items-center space-y-3 p-5">
+
+                <div className="text-center">
+                  <h1 className="text-xl font-bold">UCSD Alpha Kappa Psi</h1>
+                  <h1 className="text-lg font-bold">(Website Revamp)</h1>
+                </div>
+
+                <div className="w-9/10 h-1/2 flex flex-col item-center justify-center bg-[#003366] px-5 object-contain rounded-xl">
+                  <img className="w-4/5 h-4/5 self-center object-contain" src="/akpsiLogo.png"/>
+                </div>
+
+                <div className="flex flex-wrap gap-2 justify-center">
+                  <h1 className="border-1 rounded-2xl py-1 px-4"><span>☰</span>Next.js</h1>
+                  <h1 className="border-1 rounded-2xl py-1 px-4"><span>☰</span>TypeScript</h1>
+                  <h1 className="border-1 rounded-2xl py-1 px-4"><span>☰</span>Tailwind</h1>
+                </div>
+
+                <p className="break-words">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt, molestiae ea a totam harum facilis dolor nostrum magnam officiis aspernatur debitis pariatur ullam vitae numquam? Rerum ullam aspernatur ex.</p>
+              
+              </div>
+
+            </a>
+
+            <a href="https://devpost.com/software/zot-confessions" target="_blank" rel="noopener noreferrer" 
+            className="w-3/4 min-h-1/6 max-w-150 border-2 rounded-xl hover:scale-105 transition duration-500 delay-75">
+
+              <div className="h-full flex flex-col items-center space-y-3 p-5">
+
+                <div className="text-center">
+                  <h1 className="text-xl font-bold">Zot Confessions</h1>
+                  <h1 className="text-lg font-bold">(React Native)</h1>
+                </div>
+
+                <div className="w-9/10 h-1/2 flex flex-col item-center justify-center bg-[#ffffff] px-5 object-contain rounded-xl">
+                  <img className="w-4/5 h-4/5 self-center object-contain" src="/zotconfessions.jpg"/>
+                </div>
+
+                <div className="flex flex-wrap gap-2 justify-center">
+                  <h1 className="border-1 rounded-2xl py-1 px-4"><span>☰</span>React Native</h1>
+                  <h1 className="border-1 rounded-2xl py-1 px-4"><span>☰</span>JavaScript</h1>
+                  <h1 className="border-1 rounded-2xl py-1 px-4"><span>☰</span>Python</h1>
+                  <h1 className="border-1 rounded-2xl py-1 px-4"><span>☰</span>Flask</h1>
+                </div>
+
+                <p className="break-words">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt, molestiae ea a totam harum facilis dolor nostrum magnam officiis aspernatur debitis pariatur ullam vitae numquam? Rerum ullam aspernatur ex.</p>
+              
+              </div>
+
+            </a>
+
+            <a href="https://devpost.com/software/zot-confessions" target="_blank" rel="noopener noreferrer" 
+            className="w-3/4 min-h-1/6 max-w-150 border-2 rounded-xl hover:scale-105 transition duration-500 delay-75">
+
+              <div className="h-full flex flex-col items-center space-y-3 p-5">
+
+                <div className="text-center">
+                  <h1 className="text-xl font-bold">LevelUpTogether</h1>
+                  <h1 className="text-lg font-bold"></h1>
+                </div>
+
+                <div className="w-9/10 h-1/2 flex flex-col item-center justify-center bg-[#ffffff] px-5 object-contain rounded-xl">
+                  <img className="w-4/5 h-4/5 self-center object-contain" src="/zotconfessions.jpg"/>
+                </div>
+
+                <div className="flex flex-wrap gap-2 justify-center">
+                  <h1 className="border-1 rounded-2xl py-1 px-4"><span>☰</span>React Native</h1>
+                  <h1 className="border-1 rounded-2xl py-1 px-4"><span>☰</span>JavaScript</h1>
+                  <h1 className="border-1 rounded-2xl py-1 px-4"><span>☰</span>Firebase</h1>
+                </div>
+
+                <p className="break-words">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt, molestiae ea a totam harum facilis dolor nostrum magnam officiis aspernatur debitis pariatur ullam vitae numquam? Rerum ullam aspernatur ex.</p>
+              
+              </div>
+
+            </a>
+
+          </div>
+
         </div>
+        
       </section>
 
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,47 @@
+'use client';
+import { useState } from 'react';
+
+{/** add a hover effect that moves when you move mouse over */}
 export const Navbar = () => {
+
+  const [toggled, setToggled] = useState(false);
+
   return (
-    <>
-        <nav className="fixed top-0 left-0 w-full bg-black shadow-md z-50 flex space-x-4">
+        <nav className="fixed top-0 left-0 w-full bg-black shadow-md z-50 flex justify-between py-4 px-3 space-x-2">
+          
+          <div><a href="#about">Alan Jiang</a></div>
+          
 
-          <div><a href="#top">Home</a></div> {/*make this a logo?*/}
+          <input id="menu-toggle" type="checkbox" className="peer hidden" />
 
-          <div><a href="#projects">Projects</a></div>
+          <div className="md:hidden cursor-pointer text-white" onClick={() => setToggled(!toggled)}>
+            ☰
+          </div>
+          
+          {toggled ? (
+
+            <ul className="absolute top-10 left-0 w-full bg-black text-center space-y-2">
+              <li className="md:hidden px-10 pt-4 opacity-80 hover:opacity-100 transition-opacity">
+                  <a href="#about" className="text-lg" onClick={() => setToggled(!toggled)}>About</a>
+              </li>
+            <li className="md:hidden px-10 py-4 opacity-80 hover:opacity-100 transition-opacity">
+              
+              <a href="#projects" className="text-lg" onClick={() => setToggled(!toggled)}>Projects</a>
+            </li>
+          </ul>
+
+          ):(
+            null
+          ) }
+          
+
+          <ul className="hidden md:flex space-x-2">
+            <div><a href="#about">About</a></div> {/*make this a logo?*/}
+            <div><a href="#projects">Projects</a></div>
+          </ul>
 
         </nav>
-    </>
+
   )
 }
 
